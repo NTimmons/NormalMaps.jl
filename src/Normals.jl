4ea6fcb9e82bc7b0b8417d8d22a347b4da2f5c7a
root <root@DESKTOP-S9CHSCT.localdomain>
2019-05-25 14:15:29 +0100

    Added Normal Blending

--- src/Normals.jl
+++ src/Normals.jl
@@ -2,20 +2,38 @@ module Normals
 
 using Images
 using LinearAlgebra
+## Helpers
+############
+
+function ToArray(texel)
+    [texel.r, texel.g, texel.b] 
+end
+
+function ToRGB(texel)
+    RGB(texel[1], texel[2], texel[3])
+end
 
 function RGBSum(_in)
     _in.r + _in.g + _in.b 
-end
-
-function ZeroOneSpaceMapping(_rgb)
-    col = [ (_rgb[1]+1.0)*0.5,  (_rgb[2]+1.0)*0.5,  1.0]
 end
 
 function IdentitySpaceMapping(_rgb)
     _rgb
 end
 
-function NormalGen_Sobel(_img; filter=[-0.5, 0, 0.5], SpaceMapping=ZeroOneSpaceMapping)
+function ToNormalSpace(_rgb)
+    col = [ (_rgb[1]+1.0)*0.5,  (_rgb[2]+1.0)*0.5,  (_rgb[3]+1.0)*0.5]
+end
+
+# Normal data helper functions
+export IdentitySpaceMapping, ToNormalSpace
+export RGBSum
+export ToRGB, ToArray
+
+### Generate Normal Map From Height/Colour
+###########################################
+
+function NormalGen_Sobel(_img; filter=[-0.5, 0, 0.5], SpaceMapping=ToNormalSpace)
     width, height = size(_img);
     oldimg        = RGB{Float64}.(_img)
     newimg        = copy(RGB{Float64}.(_img))
@@ -49,7 +67,37 @@ NormalGen = NormalGen_Sobel
 # Normal generation functions
 export NormalGen_Sobel, NormalGen
 
-# Normal data formatting functions
-export ZeroOneSpaceMapping, IdentitySpaceMapping
+## Merging Normal Maps
+#######################
+
+## Based on RNM from https://blog.selfshadow.com/publications/blending-in-detail/
+function NormalBlend_RNM(_n1, _n2; ToNormalFunc=ToNormalSpace )
+    n1 = _n1 .* [  2.0,   2.0,  2.0]  .+ [ -1.0, -1.0,   0.0]
+    n2 = _n2 .* [ -2.0,  -2.0,  2.0]  .+ [  1.0,  1.0,  -1.0]
+    n  = n1*dot(n1, n2)/n1[3] - n2;
+    ToNormalFunc(normalize(n))
+end
+
+# Precondition: Assert that input normals are stored packed in 0-1 range by: (xyz + 1.0) * 0.5
+function BlendNormalsRNM(inputOne, inputTwo; BlendFunc=NormalBlend_RNM)
+    width, height = size(inputOne);
+    newimg = copy(RGB{Float64}.(inputOne))
+    inputOneF = RGB{Float64}.(inputOne)
+    inputTwoF = RGB{Float64}.(inputTwo)
+    
+    for y in 1:height
+        for x in 1:width
+            texelOne = ToArray(inputOneF[x,y])
+            texelTwo = ToArray(inputTwoF[x,y])
+
+            texelOut = BlendFunc(texelOne, texelTwo)
+            newimg[x,y] = ToRGB( texelOut )
+        end
+    end
+    
+    newimg
+end
+
+export NormalBlend_RNM, BlendNormalsRNM
 
 end # module
